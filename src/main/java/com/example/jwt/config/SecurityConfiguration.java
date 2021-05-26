ecbd282cc8bb7397ffe678faf52a7a7bbaa224d2
JeongYongWoo <aba1430@naver.com>
2021-05-26 11:47:19 +0900

    [Create] SecurityConfiguration

--- src/main/java/com/example/jwt/config/SecurityConfiguration.java
+++ src/main/java/com/example/jwt/config/SecurityConfiguration.java
@@ -13,6 +13,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/api/**");
+        web.ignoring().antMatchers("/h2-console/**","/user/**");
     }
 }
